793102cc3f7fcc48e71ebc52bf8cb0737415f49d
Maciej Kempin <maciej.kempin@gmail.com>
2022-07-07 21:50:25 +0100

    being more precise

--- source/list_envs.rb
+++ source/list_envs.rb
@@ -3,4 +3,10 @@ require 'json'
 puts "Let's see what we got here."
 puts ENV.inspect
 
+puts "ENV['MY_NOT_IMPORTANT_VARIABLE']"
+puts ENV['MY_NOT_IMPORTANT_VARIABLE']
+
+puts "ENV['GLOBAL_NOT_IMPORTANT']"
+puts ENV['GLOBAL_NOT_IMPORTANT']
+
 puts JSON.parse(ENV['SECRET_KEY_A'])
